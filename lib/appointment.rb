9537f0403ebc917be891fbf3150a0dae017a3d0e
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-21 15:14:59 -0400

    has many through

---
+++ lib/appointment.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  def initialize (date, doctor)
+    @date = date
+    @doctor = doctor
+    self.doctor.appointments << self
+  end
+
+
+
+
+end
